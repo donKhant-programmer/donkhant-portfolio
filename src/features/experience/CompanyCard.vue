0a649000ab38aced7257298929bf688eb2b1ca4a
Don Khant <donkhant1@gmail.com>
2026-04-21 23:42:45 +0630

    feat: experience page

---
+++ src/features/experience/CompanyCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="bg-slate-800 p-6 rounded-xl hover:scale-[1.02] transition cursor-pointer"
+    @click="goToDetail"
+  >
+    <h2 class="text-xl font-semibold mb-2">
+      {{ company.name }}
+    </h2>
+
+    <p class="text-indigo-400 text-sm mb-1">
+      {{ company.role }}
+    </p>
+
+    <p class="text-gray-400 text-sm">
+      {{ company.duration }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import type { Company } from "@/data/companies";
+
+const props = defineProps<{
+  company: Company;
+}>();
+
+const router = useRouter();
+
+const goToDetail = () => {
+  router.push(`/experience/${props.company.id}`);
+};
+</script>
---
+++ src/pages/ExperiencePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="py-20">
+    <h1 class="text-3xl font-bold mb-10">Experience</h1>
+
+    <div class="grid md:grid-cols-2 gap-6">
+      <CompanyCard v-for="c in companies" :key="c.id" :company="c" />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { companies } from "@/data/companies";
+import CompanyCard from "@/features/experience/CompanyCard.vue";
+</script>
